36d5451f216b17c0ca7371b48f32e4fefedf0a0b
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-07 23:59:07 +0200

    finished the search functionality

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,7 @@ const addTaskBtn = document.querySelector('button.add-btn')
 let tasks = []
 if (localStorage.getItem('tasks')) {
   tasks = JSON.parse(localStorage.getItem('tasks'))
-  for (let task of tasks)
-    displayTask(task)
+  displayAllTasks(tasks)
 }
 let tasksCounter = tasks.length
 function addTask() {
@@ -20,6 +19,22 @@ function addTask() {
   localStorage.setItem('tasks', JSON.stringify(tasks))
   addTaskInput.value = null
   displayTask(task)
+}
+function searchTask() {
+  const searchedTasks = []
+  for (let task of tasks) {
+    if (task.title.toLowerCase().includes(searchTaskInput.value.toLowerCase())) {
+      searchedTasks.push(task)
+    }
+  }
+  tasksContainer.innerHTML = ''
+  if (searchedTasks.length > 0)
+    displayAllTasks(searchedTasks)
+  else if (searchTaskInput.value) {
+    displayTask({ id: -1, title: 'No such task' }, false)
+  }
+  else
+    displayAllTasks(tasks)
 }
 function doElement(value = null, element, ...classesToAdd) {
   const htmlElement = document.createElement(element)
@@ -35,32 +50,42 @@ function appendElementsToParent(parent, ...children) {
   for (let child of children)
     parent.append(child)
 }
-function displayTask(theTask) {
+function doTask(theTask, option = true) {
   const task = doElement(null, 'div', 'task');
   const taskId = doElement(theTask.id, 'div', 'task-id');
   const taskTitle = doElement(theTask.title, 'div', 'task-title')
   const taskActions = doElement(null, 'div', 'task-actions')
   const elementsToAppend = [taskId, taskTitle, taskActions]
-  for (let i = 0; i < 3; i++) {
-    const actionHolder = doElement(null, 'div', 'action-holder')
-    const icon = doElement(null, 'i')
-    actionHolder.append(icon)
-    if (i == 0) {
-      actionHolder.classList.add('update')
-      addClassesToElement(actionHolder, 'update')
-      addClassesToElement(icon, 'fa-solid', 'fa-pen', 'update-icon')
-    } else if (i == 1) {
-      actionHolder.classList.add('delete')
-      addClassesToElement(actionHolder, 'delete')
-      addClassesToElement(icon, ...'fa-solid fa-trash delete-icon'.split(' '))
-    } else {
-      addClassesToElement(actionHolder, 'check')
-      addClassesToElement(icon, ...'fa-solid fa-check check-icon'.split(' '))
+  if (option) {
+    for (let i = 0; i < 3; i++) {
+      const actionHolder = doElement(null, 'div', 'action-holder')
+      const icon = doElement(null, 'i')
+      actionHolder.append(icon)
+      if (i == 0) {
+        actionHolder.classList.add('update')
+        addClassesToElement(actionHolder, 'update')
+        addClassesToElement(icon, 'fa-solid', 'fa-pen', 'update-icon')
+      } else if (i == 1) {
+        actionHolder.classList.add('delete')
+        addClassesToElement(actionHolder, 'delete')
+        addClassesToElement(icon, ...'fa-solid fa-trash delete-icon'.split(' '))
+      } else {
+        addClassesToElement(actionHolder, 'check')
+        addClassesToElement(icon, ...'fa-solid fa-check check-icon'.split(' '))
+      }
+      taskActions.append(actionHolder)
     }
-    taskActions.append(actionHolder)
   }
   appendElementsToParent(task, ...elementsToAppend)
+  return task
+}
+function displayTask(theTask, option) {
+  const task = doTask(theTask, option)
   tasksContainer.append(task)
 }
-
+function displayAllTasks(tasks) {
+  for (let task of tasks)
+    displayTask(task)
+}
 addTaskBtn.addEventListener('click', addTask)
+searchTaskInput.addEventListener('input', searchTask)
